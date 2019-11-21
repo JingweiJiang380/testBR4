fa8380308c0a1381994ef889e1acda191ea6c6b5
JingweiJiang380 <56660572+JingweiJiang380@users.noreply.github.com>
2019-11-20 23:34:35 -0500

    newstuff

---
+++ map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamluZ3dlaWppYW5nIiwiYSI6ImNrMXV5bm0weDE2cHQzbWsxZndpZGVzdjMifQ._NtAZAWX72-MN9tn7K_Oiw';
+
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/jingweijiang/ck1v0vfa97vzv1cqstuyqby0v'
+
+});
